c2d6f9e8bacc651c601150d2d1814467fdbd16ef
Long Tran <plongtran239@gmail.com>
2025-02-13 11:34:45 +0700

    update

--- src/app/(root)/(course)/courses/[nameId]/schedule/components/schedule.tsx
+++ src/app/(root)/(course)/courses/[nameId]/schedule/components/schedule.tsx
@@ -230,7 +230,7 @@ const Schedule = ({
 
             <div className="flex items-center gap-2">
               <div className="h-5 w-5 bg-red-500"></div>
-              <span>Đã được đặt</span>
+              <span>Đã đủ số lượng</span>
             </div>
           </div>
 
